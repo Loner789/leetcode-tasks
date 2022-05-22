890a185bfc8369edfbd49b4e73656cb2c97dcdf9
Dmitry <royalmailforloner@gmail.com>
2022-05-22 03:48:38 +0300

    feat: add remove duplicates from sorted array

---
+++ easy/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+var removeDuplicates = function (nums) {
+  if (nums.length == 0) return 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i < nums.length - 1 && nums[i] == nums[i + 1]) continue;
+    nums[count] = nums[i];
+    count++;
+  }
+  return count;
+};
