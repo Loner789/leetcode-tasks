768b693b51ded2a2c04df066d1146f9f9e95b806
Dmitry <royalmailforloner@gmail.com>
2022-05-22 22:17:23 +0300

    feat: add palindrome-number solution

---
+++ easy/palindrome-number.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/palindrome-number/
+
+var isPalindrome = function(x) {
+    if (x.length === 0 || x < 0) return false;
+    let i = String(x).split('').reverse().join('');
+    if (x == i) return true;
+    else return false
+}
