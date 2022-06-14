bd9692bbd925d0361937ab56a11edee89da53eeb
Dmitry <royalmailforloner@gmail.com>
2022-06-14 16:28:42 +0300

    test
    try cherry-pick

--- easy/running-sum-of-1d-array.js
+++ easy/running-sum-of-1d-array.js
@@ -21,3 +21,12 @@ var runningSum = function(nums) {
 
     return nums;
 };
+
+// Second variant of solution
+var runningSum = function(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i - 1];
+    }
+
+    return nums;
+};
