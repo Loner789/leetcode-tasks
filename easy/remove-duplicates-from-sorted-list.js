7d490ef7c1b687a30c0e0af73450e6254d4ee7eb
Dmitry <royalmailforloner@gmail.com>
2022-06-29 02:01:51 +0300

    feat: add remove-duplicates-from-sorted-list solution

---
+++ easy/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+var deleteDuplicates = function (head) {
+  let current = head;
+  while (current) {
+    current.val === current.next?.val
+      ? (current.next = current.next.next)
+      : (current = current.next);
+  }
+
+  return head;
+};
