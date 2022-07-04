99d1722cb64419b903643e3bcc78f981cff3bd63
Dmitry <royalmailforloner@gmail.com>
2022-07-04 17:46:58 +0300

    feat: add same-tree smolution

---
+++ easy/same-tree.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/same-tree/
+
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
